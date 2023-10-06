d1f036c7c993a7681bea42ba42bad26b52756b49
ssvitlana31 <130747746+ssvitlana31@users.noreply.github.com>
2023-10-06 13:01:44 +0300

    hw

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,106 @@
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { PixabayAPI } from './unsplash-api';
+import { PixabayAPI } from './pixabay-api';
+import makeGalleryCard from '../templates/gallery-card.hbs';
 
-const pixabay = new PixabayAPI(20);
+const pixabay = new PixabayAPI();
+const listEl = document.querySelector('.gallery');
+const formEl = document.querySelector('.search-form');
+const loadMoreEl = document.querySelector('.btn-load-more');
 
-pixabay.getPhotos().then(console.log);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-// const listEl = document.querySelector('.gallery');
-// const formEl = document.querySelector('.search-form');
-// const inputEl = document.querySelector('[name="searchQuery"]');
-// const loadMoreEl = document.querySelector('.btn-load-more');
-// const searchEl = document.querySelector('.btn-submit');
+const onLoadMoreClick = async () => {
+  try {
+    pixabay.page += 1;
 
-// const perPage = 40;
-// let page = 1;
-// let searchQuery = '';
-// let submitClick = false;
+    const response = await pixabay.getPhotos();
+    const { hits, totalHits } = response;
+    const totalPage = Math.ceil(totalHits / 40);
 
-// let gallery = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
+    const galleryMarkup = makeGalleryCard(hits);
+    listEl.insertAdjacentHTML('beforeend', galleryMarkup);
+    lightbox.refresh();
 
-// formEl.addEventListener('submit', onSubmit);
+    if (totalPage === pixabay.page) {
+      removeBtnLoadMore();
+    }
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .lastElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const onSubmit = async e => {
+  e.preventDefault();
+  const searchQuery = e.currentTarget.elements.searchQuery.value;
+  pixabay.query = searchQuery.trim();
+  pixabay.page = 1;
+
+  try {
+    if (!pixabay.query) {
+      Notify.failure('Sorry, enter a valid query. Please try again.');
+      return;
+    }
+    const response = await pixabay.getPhotos();
+
+    const { hits, totalHits } = response;
+
+    const totalPage = Math.ceil(totalHits / 40);
+
+    if (totalHits > 0) {
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
+
+    if (totalPage === 0) {
+      removeBtnLoadMore();
+      listEl.innerHTML = '';
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    if (totalPage === 1) {
+      const galleryMarkup = makeGalleryCard(hits);
+      listEl.innerHTML = galleryMarkup;
+      lightbox.refresh();
+      return;
+    }
+
+    const galleryMarkup = makeGalleryCard(hits);
+    listEl.innerHTML = galleryMarkup;
+    lightbox.refresh();
+    addBtnLoadMore();
+  } catch (error) {
+    console.log('No result for your request', error);
+  }
+};
+
+const removeBtnLoadMore = e => {
+  loadMoreEl.classList.add('is-hidden');
+  loadMoreEl.removeEventListener('click', onLoadMoreClick);
+  Notify.info("We're sorry, but you've reached the end of search results.");
+};
+
+const addBtnLoadMore = () => {
+  loadMoreEl.classList.remove('is-hidden');
+  loadMoreEl.addEventListener('click', onLoadMoreClick);
+};
+
+formEl.addEventListener('submit', onSubmit);
 
 // async function onSubmit(e) {
 //   e.preventDefault();
@@ -73,7 +150,4 @@ pixabay.getPhotos().then(console.log);
 //     .join('');
 // }
 
-// function renderPage(images) {
-//   const markup = imageTemplate(images);
-//   listEl.insertAdjacentHTML('beforeend', markup);
-// }
+//
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,27 +2,27 @@ import axios from 'axios';
 
 export class PixabayAPI {
   #BASE_URL = 'https://pixabay.com/api/';
-  #API_KEY = '38181676-c389c3ce2b1eee7a286cf8f0e';
+  #API_KEY = '39829920-01ffc9c03864e4c35c3c45cf7';
 
   constructor() {
-    this.searchQuery = '';
+    this.query = '';
     this.page = 1;
-    this.perPage = this.perPage;
+    this.perPage = 40;
   }
 
-  getPhotos() {
-    const searchParams = {
+  async getPhotos() {
+    const PARAMS = new URLSearchParams({
       key: this.#API_KEY,
-      q: this.searchQuery,
+      q: this.query,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
       per_page: this.perPage,
       page: this.page,
-    };
+    });
 
-    return axios
-      .get(`${this.#BASE_URL}?`, { params: searchParams })
-      .then(res => res.data);
+    const url = `${this.#BASE_URL}?${PARAMS}`;
+    const res = await axios.get(url);
+    return res.data;
   }
 }
